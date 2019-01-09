fdede80c22e786a6717990dd856fb9510f0bcbd5
Fabian Bühler <17296905+st116290@users.noreply.github.com>
2019-01-09 10:49:33 +0100

    Add packaging into different formats
    Based on marcobotto.com/blog/compiling-and-bundling-typescript-libraries-with-webpack/

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-    entry: "./src/index.ts",
+    entry: {
+        "grapheditor-webcomponent": "./src/index.ts",
+        //"grapheditor-webcomponent.min": "./src/index.ts",
+    },
 
     // Enable sourcemaps for debugging webpack's output.
     devtool: "source-map",
@@ -11,17 +15,40 @@ module.exports = {
         extensions: [".webpack.js", ".web.js", ".ts", ".js"]
     },
 
+    /*
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true,
+                terserOptions: {
+                    // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
+                }
+            }),
+        ],
+    },
+    */
+
     module: {
         rules: [
             {
                 test: /\.[jt]s$/,
-                use: "awesome-typescript-loader",
-                exclude: path.resolve(__dirname, "node_modules")
+                use: [{
+                    loader: "awesome-typescript-loader",
+                    options: {
+                      declaration: false,
+                    }
+                }],
+                exclude: path.resolve(__dirname, "node_modules"),
             }
         ]
     },
     output: {
-        filename: "grapheditor-webcomponent.js",
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, '_bundles'),
+        filename: '[name].js',
+        libraryTarget: 'umd',
+        library: 'grapheditor-webcomponent',
+        umdNamedDefine: true
     },
 };
