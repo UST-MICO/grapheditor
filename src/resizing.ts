f91ed9f545cc4caf423a6b2feef31b7799022f63
Fabian Bühler <17296905+buehlefs@users.noreply.github.com>
2020-05-18 21:13:21 +0200

    Add basic resize handlers for all directions

--- src/resizing.ts
+++ src/resizing.ts
@@ -1,6 +1,8 @@
 import GraphEditor from './grapheditor';
-import { Selection, select } from 'd3-selection';
+import { Selection, select, event } from 'd3-selection';
+import { drag } from 'd3-drag';
 import { Rect, removeAllChildNodes, copyTemplateSelectionIntoNode } from './util';
+import { Point } from './edge';
 
 
 export interface ResizeOverlayOptions {
@@ -22,6 +24,9 @@ interface ResizeHandle {
     x: number;
     y: number;
 }
+
+type ResizeHandler = (dx: number, dy: number, rect: Rect) => Rect;
+
 
 // eslint-disable-next-line complexity
 function resizeHandlesFromOptions(options: ResizeOverlayOptions|null, bbox: Rect): ResizeHandle[] {
@@ -104,6 +109,16 @@ function resizeHandlesFromOptions(options: ResizeOverlayOptions|null, bbox: Rect
     return handles;
 }
 
+function clampDelta(d: number, value: number, min: number, max: number): number {
+    if (d > 0) {
+        return Math.min(d, max - value);
+    }
+    if (d < 0) {
+        return -Math.min(-d, value - min);
+    }
+    return d;
+}
+
 export class ResizingManager {
     readonly graphEditor: GraphEditor;
 
@@ -115,17 +130,18 @@ export class ResizingManager {
     private overlayGroup: Selection<SVGGElement, any, any, any>;
 
     private resizeOptions: Map<string, ResizeOverlayOptions> = new Map();
+    private currentlyResizing: Map<string, Rect> = new Map();
 
     private resizeOverlays: string[] = [];
 
     constructor(graphEditor: GraphEditor) {
         this.graphEditor = graphEditor;
-        this.svgChange = (event) => {this.initializeGraph(this.graphEditor.getGraphGroup());};
+        this.svgChange = (changeEvent: CustomEvent) => { this.initializeGraph(this.graphEditor.getGraphGroup()); };
         this.graphEditor.addEventListener('svginitialized', this.svgChange);
         this.initializeGraph(this.graphEditor.getGraphGroup());
 
-        this.nodePositionChange = (event) => {
-            const nodeId = event.detail.node.id;
+        this.nodePositionChange = (positionChangeEvent: CustomEvent) => {
+            const nodeId = positionChangeEvent.detail.node.id;
             if (this.isResizeOverlayVisible(nodeId)) {
                 this.overlayGroup.selectAll<SVGGElement, string>('g.resize-overlay')
                     .data<string>(this.resizeOverlays)
@@ -206,7 +222,7 @@ export class ResizingManager {
     }
 
     private updateOverlay(overlaySelection: Selection<SVGGElement, any, any, any>, nodeId: string) {
-        const bbox = this.graphEditor.getNodeBBox(nodeId);
+        const bbox: Rect = this.currentlyResizing.get(nodeId) ?? this.graphEditor.getNodeBBox(nodeId);
         const options: ResizeOverlayOptions = this.resizeOptions.get(nodeId);
 
         overlaySelection.select('rect.outline')
@@ -219,8 +235,10 @@ export class ResizingManager {
 
         const templateRegistry = this.graphEditor.staticTemplateRegistry;
 
-        overlaySelection.selectAll<SVGGElement, ResizeHandle>('g.resizeHandle')
-            .data<ResizeHandle>(resizeHandlesFromOptions(options, bbox))
+        const resizeHandles = resizeHandlesFromOptions(options, bbox);
+
+        overlaySelection.selectAll<SVGGElement, ResizeHandle>('g.resize-handle')
+            .data<ResizeHandle>(resizeHandles)
             .join(
                 enter => {
                     return enter.append('g')
@@ -257,17 +275,71 @@ export class ResizingManager {
                     .classed('bottom', handle.type.startsWith('bottom'))
                     .classed('left', handle.type.endsWith('left'))
                     .classed('right', handle.type.endsWith('right'));
+            })
+            .call(this.updateResizeHandlePositions.bind(this))
+            .call(
+                drag<SVGGElement, ResizeHandle, {handler: ResizeHandler; start: Point; startRect: Rect}>()
+                    .subject((handle) => {
+                        if (this.currentlyResizing.has(nodeId)) {
+                            return;
+                        }
+                        // eslint-disable-next-line no-shadow
+                        const bbox = this.graphEditor.getNodeBBox(nodeId);
+                        this.currentlyResizing.set(nodeId, bbox);
+                        const handler = this.resizeHandlerFromHandle(nodeId, handle);
+                        return {
+                            handler: handler,
+                            start: {x: event.x, y: event.y},
+                            startRect: bbox,
+                        };
+                    })
+                    .on('drag', () => {
+                        const resizeHandler: ResizeHandler = event.subject.handler;
+                        const start: Point = event.subject.start;
+                        const startRect: Rect = event.subject.startRect;
+                        const newRect = resizeHandler(event.x - start.x, event.y - start.y, startRect);
+                        this.currentlyResizing.set(nodeId, newRect);
+
+                        this.updateOverlayDimensions(overlaySelection, nodeId, resizeHandlesFromOptions(options, newRect));
+                    })
+                    .on('end', () => {
+                        this.currentlyResizing.delete(nodeId);
+
+                        // eslint-disable-next-line no-shadow
+                        const bbox = this.graphEditor.getNodeBBox(nodeId);
+
+                        this.updateOverlayDimensions(overlaySelection, nodeId, resizeHandlesFromOptions(options, bbox));
+                    })
+            );
+    }
 
 
-                const translate = `translate(${handle.x},${handle.y})`;
+    private updateOverlayDimensions(overlaySelection: Selection<SVGGElement, string, SVGGElement, any>, nodeId, resizeHandles: ResizeHandle[]) {
+        const bbox: Rect = this.currentlyResizing.get(nodeId) ?? this.graphEditor.getNodeBBox(nodeId);
 
-                // handle rotation
-                if (handle.rotation != null && handle.rotation !== 0) {
-                    handleSelection.attr('transform', `${translate} rotate(${handle.rotation})`);
-                } else {
-                    handleSelection.attr('transform', translate);
-                }
-            });
+        overlaySelection.select('rect.outline')
+            .attr('x', bbox.x)
+            .attr('y', bbox.y)
+            .attr('width', bbox.width)
+            .attr('height', bbox.height);
+
+        overlaySelection.selectAll<SVGGElement, ResizeHandle>('g.resize-handle')
+            .data<ResizeHandle>(resizeHandles)
+            .call(this.updateResizeHandlePositions.bind(this));
+    }
+
+    private updateResizeHandlePositions(handleSelection: Selection<SVGGElement, ResizeHandle, any, any>) {
+        handleSelection.attr('transform', (handle) => {
+            const translate = `translate(${handle.x},${handle.y})`;
+
+            console.log('translate', translate);
+            // handle rotation
+            if (handle.rotation != null && handle.rotation !== 0) {
+                return `${translate} rotate(${handle.rotation})`;
+            } else {
+                return translate;
+            }
+        });
     }
 
     private updateOverlayPositions(overlaySelection: Selection<SVGGElement, string, SVGGElement, any>) {
@@ -275,6 +347,118 @@ export class ResizingManager {
             const node = this.graphEditor.getNode(nodeId);
             return `translate(${node.x},${node.y})`;
         });
+    }
+
+
+    private resizeHandlerFromHandle(nodeId: string, handle: ResizeHandle): ResizeHandler {
+        const options = this.resizeOptions.get(nodeId);
+
+        const minWidth = 10;
+        const minHeight = 10;
+        const maxWidth = Infinity;
+        const maxHeight = Infinity;
+
+        if (handle.type === 'top-left') {
+            return (dx: number, dy: number, rect: Rect) => {
+                const cdx = -clampDelta(-dx, rect.width, minWidth, maxWidth);
+                const cdy = -clampDelta(-dy, rect.height, minHeight, maxHeight);
+                return {
+                    x: rect.x + cdx,
+                    y: rect.y + cdy,
+                    width: rect.width - cdx,
+                    height: rect.height - cdy,
+                };
+            };
+        }
+
+        if (handle.type === 'top-right') {
+            return (dx: number, dy: number, rect: Rect) => {
+                const cdx = clampDelta(dx, rect.width, minWidth, maxWidth);
+                const cdy = -clampDelta(-dy, rect.height, minHeight, maxHeight);
+                return {
+                    x: rect.x,
+                    y: rect.y + cdy,
+                    width: rect.width + cdx,
+                    height: rect.height - cdy,
+                };
+            };
+        }
+
+        if (handle.type === 'bottom-left') {
+            return (dx: number, dy: number, rect: Rect) => {
+                const cdx = -clampDelta(-dx, rect.width, minWidth, maxWidth);
+                const cdy = clampDelta(dy, rect.height, minHeight, maxHeight);
+                return {
+                    x: rect.x + cdx,
+                    y: rect.y,
+                    width: rect.width - cdx,
+                    height: rect.height + cdy,
+                };
+            };
+        }
+
+        if (handle.type === 'bottom-right') {
+            return (dx: number, dy: number, rect: Rect) => {
+                const cdx = clampDelta(dx, rect.width, minWidth, maxWidth);
+                const cdy = clampDelta(dy, rect.height, minHeight, maxHeight);
+                return {
+                    x: rect.x,
+                    y: rect.y,
+                    width: rect.width + cdx,
+                    height: rect.height + cdy,
+                };
+            };
+        }
+
+        if (handle.type === 'top') {
+            return (dx: number, dy: number, rect: Rect) => {
+                const cdy = -clampDelta(-dy, rect.height, minHeight, maxHeight);
+                return {
+                    x: rect.x,
+                    y: rect.y + cdy,
+                    width: rect.width,
+                    height: rect.height - cdy,
+                };
+            };
+        }
+
+        if (handle.type === 'bottom') {
+            return (dx: number, dy: number, rect: Rect) => {
+                const cdy = clampDelta(dy, rect.height, minHeight, maxHeight);
+                return {
+                    x: rect.x,
+                    y: rect.y,
+                    width: rect.width,
+                    height: rect.height + cdy,
+                };
+            };
+        }
+
+        if (handle.type === 'left') {
+            return (dx: number, dy: number, rect: Rect) => {
+                const cdx = -clampDelta(-dx, rect.width, minWidth, maxWidth);
+                return {
+                    x: rect.x + cdx,
+                    y: rect.y,
+                    width: rect.width - cdx,
+                    height: rect.height,
+                };
+            };
+        }
+
+        if (handle.type === 'right') {
+            return (dx: number, dy: number, rect: Rect) => {
+                const cdx = clampDelta(dx, rect.width, minWidth, maxWidth);
+                return {
+                    x: rect.x,
+                    y: rect.y,
+                    width: rect.width + cdx,
+                    height: rect.height,
+                };
+            };
+        }
+
+        return (dx, dy, rect) => rect;
     }
 
 }
