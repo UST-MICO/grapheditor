43fab8ce0e2e4154899e92e06cd337fcc99798e5
Fabian Bühler <17296905+buehlefs@users.noreply.github.com>
2021-09-05 23:35:04 +0200

    Start upgrade to new d3 version

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,8 @@
 const path = require('path');
-const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: {
         "grapheditor-webcomponent": "./src/index.ts",
-        //"grapheditor-webcomponent.min": "./src/index.ts",
     },
 
     // Enable sourcemaps for debugging webpack's output.
@@ -15,31 +13,11 @@ module.exports = {
         extensions: [".webpack.js", ".web.js", ".ts", ".js"]
     },
 
-    /*
-    optimization: {
-        minimizer: [
-            new TerserPlugin({
-                cache: true,
-                parallel: true,
-                sourceMap: true,
-                terserOptions: {
-                    // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
-                }
-            }),
-        ],
-    },
-    */
-
     module: {
         rules: [
             {
                 test: /\.[jt]s$/,
-                use: [{
-                    loader: "awesome-typescript-loader",
-                    options: {
-                      declaration: false,
-                    }
-                }],
+                use: "ts-loader",
                 exclude: path.resolve(__dirname, "node_modules"),
             }
         ]
